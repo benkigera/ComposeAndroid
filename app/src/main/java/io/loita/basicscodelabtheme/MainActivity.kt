badfc382cc2c4b8a31898d46b9630c66bdd46a2c
benkigera <benkigera@gmail.com>
2022-11-11 11:47:13 +0300

    Rename theme

--- app/src/main/java/io/loita/basicscodelabtheme/MainActivity.kt
+++ app/src/main/java/io/loita/basicscodelabtheme/MainActivity.kt
@@ -1,25 +1,32 @@
 package io.loita.basicscodelabtheme
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import io.loita.basicscodelabtheme.ui.theme.BasicsCodelabThemeTheme
+import io.loita.basicscodelabtheme.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabThemeTheme {
+            BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -107,31 +114,40 @@ private fun Greeting(name: String) {
     }
 }
 
+
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("benkigera", "wesley", "felix")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(
+        modifier = modifier.padding(vertical = 4.dp)
+    ) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
 }
 
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview()
 @Composable
 fun DefaultPreview() {
-    BasicsCodelabThemeTheme {
+    BasicsCodelabTheme {
         //OnBoardingScreen(onContinueClicked = {})
     }
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+    widthDp = 480,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Composable
-fun MyAppPreview() {
-    BasicsCodelabThemeTheme {
+fun MyAppPreview(
+) {
+    BasicsCodelabTheme {
         MyApp(Modifier.fillMaxSize())
     }
 }
